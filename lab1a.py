386e6bdccaf6a36c7bda47c73b6786ffcc02ab24
Tiayyba Riaz <121324609+tiayyba@users.noreply.github.com>
2024-06-18 23:19:22 -0400

    Create lab1a.py

---
+++ lab1a.py
@@ -0,0 +1,14 @@
+# The first three lines should be comments and display author name (you), name of the script (lab1a.py) and the description of the code.
+# Add comments here
+
+
+
+# Next, create a variable called message
+
+# Set the variable to equal to "Welcome to PRG101"
+
+# Print message
+
+# Use the builtin type() function and print the type of this variable.
+
+# Add another print statement that uses the variable you just created and concatenate it with the statement "Python is the future."
